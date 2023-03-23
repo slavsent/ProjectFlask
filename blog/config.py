b56e86386188447653da9cefc666947bb3b71e8e
sslava <ricar@mail.ru>
2023-03-23 22:25:15 +0300

    add change in config

--- blog/config.py
+++ blog/config.py
@@ -12,6 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+# for article
 API_URL = os.getenv('API_URL')
 
 WTF_CSRF_ENABLED = True
